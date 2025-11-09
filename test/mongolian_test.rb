2c4417d6f0f1eb7dbb7d0ddd37233cad48864b62
Andrey Zdorovtsov <zakbox@gmail.com>
2025-11-09 15:12:47 +0100

    Add additional Mongolian transliteration tests
    Added tests for Mongolian edge cases and a sample sentence to improve coverage of the Cyrillic transliteration functionality.

--- test/mongolian_test.rb
+++ test/mongolian_test.rb
@@ -4,4 +4,12 @@ class MongolianTest < Minitest::Test
   def test_mongolian
     assert_equal "Mongol khel", Cyrillic.t("Монгол хэл", :mongolian)
   end
+
+  def test_mongolian_edge_cases
+    assert_equal "OUKhTsChShShIYIEYuYa", Cyrillic.t("ӨҮХЦЧШЩЪЫЬЭЮЯ", :mongolian)
+  end
+
+  def test_mongolian_sentence
+    assert_equal "Ekh oron", Cyrillic.t("Эх орон", :mongolian)
+  end
 end
