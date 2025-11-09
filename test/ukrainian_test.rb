1bd6a99edb2af8168b1485e066ae0739948a8cfe
Andrey Zdorovtsov <zakbox@gmail.com>
2025-11-09 18:01:31 +0100

    Add test for 'Катерина' transliteration
    Added an assertion to verify that 'Катерина' is correctly transliterated to 'Kateryna' in the Ukrainian transliteration tests.

--- test/ukrainian_test.rb
+++ test/ukrainian_test.rb
@@ -4,5 +4,6 @@ class UkrainianTest < Minitest::Test
   def test_ukrainian
     assert_equal "AbvhGD", Cyrillic.t("АбвгҐД", :ukrainian)
     assert_equal "Andrii", Cyrillic.t("Андрій", :ukrainian)
+    assert_equal "Kateryna", Cyrillic.t("Катерина", :ukrainian)
   end
 end
